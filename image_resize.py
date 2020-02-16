9c9d47648f8b4885481c56e7bb65683759dc1008
vagrant <vagrant@vagrant.vm>
2020-02-16 19:22:05 +0000

    Created new file for resizing images

--- image_resize.py
+++ image_resize.py
@@ -1,9 +1,13 @@
-import os, sys
+import glob, os, sys
 import Image
 from PIL import Image
 
 size = 400, 400
 
-for infile in sys.argv[1:]:
-    outfile = os.path.splitest(infile)[0] + ".thumbnail"
-    if infile != outfile:
+infile = "static/images/DT1494.jpg"
+
+for infile in glob.glob("*.jpg"):
+    file, ext = os.path.splitest(infile)
+    im = Image.open(infile)
+    im.tumbnail(size)
+    im.save(file + ".thumbnail","JPEG")
